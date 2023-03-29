b65d09f1f2c61c01319df5afad709a6b0d981673
juhyun <zzang9ha@naver.com>
2023-03-29 22:15:20 +0900

    feat: common 상수 클래스 추가

---
+++ commons/src/main/java/constant/Constants.java
@@ -0,0 +1,11 @@
+package constant;
+
+public class Constants {
+
+    private Constants() { }
+
+    public static final long POPULAR_KEYWORD_LIST_START = 0;
+
+    public static final long POPULAR_KEYWORD_LIST_END = 9;
+
+}
