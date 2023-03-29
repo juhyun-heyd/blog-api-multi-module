d11c7da0c0178d1aa97eb238483d57b0b6b53845
juhyun <zzang9ha@naver.com>
2023-03-29 22:13:17 +0900

    refactor: Lombok @Slf4j 제거

--- blog-api/src/main/java/com/heyd/blogapi/api/handler/GlobalExceptionHandler.java
+++ blog-api/src/main/java/com/heyd/blogapi/api/handler/GlobalExceptionHandler.java
@@ -1,17 +1,19 @@
 package com.heyd.blogapi.api.handler;
 
 import com.heyd.blogapi.api.dto.ApiResult;
-import com.heyd.blogapi.domain.exception.BusinessException;
-import lombok.extern.slf4j.Slf4j;
+import com.heyd.blogapi.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     private ResponseEntity<ApiResult<?>> response(Throwable throwable, HttpStatus status) {
         var headers = new HttpHeaders();
